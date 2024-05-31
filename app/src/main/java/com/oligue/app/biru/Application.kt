d426b35e38fe2d2df8f1719495eb7f15d61080b7
Victor Guedes <victoroligue@gmail.com>
2024-05-31 15:37:19 +0100

    add initial dependencies injection

---
+++ app/src/main/java/com/oligue/app/biru/Application.kt
@@ -0,0 +1,8 @@
+package com.oligue.app.biru
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+
+class Application: Application()
---
+++ app/src/main/java/com/oligue/app/biru/core/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.oligue.app.biru.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    private val loggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BASIC)
+
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    companion object {
+        private const val BASE_URL = "https://api.openbrewerydb.org/v1/"
+    }
+}
