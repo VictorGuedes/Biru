fef3f76475e3d34cd420a8525604a7fddf01e31c
Victor Guedes <victoroligue@gmail.com>
2024-05-31 16:24:54 +0100

    change mainActivity package

--- app/src/main/java/com/oligue/app/biru/app/MainActivity.kt
+++ app/src/main/java/com/oligue/app/biru/app/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.oligue.app.biru
+package com.oligue.app.biru.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oligue.app.biru.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
