8e3490ac2e3a235493cfc7aef1a3fcd44d068f0c
Victor Guedes <victoroligue@gmail.com>
2024-06-01 10:59:00 +0100

    add more dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding {
+         enable = true
+    }
 }
 
 dependencies {
@@ -44,21 +48,27 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation ("androidx.fragment:fragment-ktx:1.7.1")
-    implementation ("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.7.1")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
 
     //Hilt
-    implementation ("com.google.dagger:hilt-android:2.48")
+    implementation("com.google.dagger:hilt-android:2.48")
+    implementation("androidx.databinding:databinding-runtime:8.4.1")
     kapt ("com.google.dagger:hilt-android-compiler:2.48")
 
     //Retrofit
-    implementation ("com.squareup.retrofit2:converter-scalars:2.7.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.retrofit2:converter-scalars:2.7.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
 
     //Paging Library 3
-    implementation ("androidx.paging:paging-runtime-ktx:3.3.0")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
