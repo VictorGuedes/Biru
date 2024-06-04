498184b6426135cc83f4367c6842eb86b77da42b
Victor Guedes <victoroligue@gmail.com>
2024-06-04 13:03:24 +0100

    add brewerie info details

--- app/src/main/java/com/oligue/app/biru/app/details/BrewerieDetailsFragment.kt
+++ app/src/main/java/com/oligue/app/biru/app/details/BrewerieDetailsFragment.kt
@@ -1,28 +1,60 @@
 package com.oligue.app.biru.app.details
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.oligue.app.biru.R
+import com.oligue.app.biru.databinding.FragmentBrewerieDetailsBinding
 
 class BrewerieDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentBrewerieDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_brewerie_details, container, false)
+        binding = FragmentBrewerieDetailsBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val brewerie = navArgs<BrewerieDetailsFragmentArgs>().value.BrewerieDetail
-        Log.d("BRE", brewerie.name)
+
+        binding.title.text = brewerie.name
+        binding.descriptionTextView.text = resources.getString(
+            R.string.Brewerie_description_info_text,
+            brewerie.brewery_type,
+            brewerie.phone
+        )
+
+        binding.descriptionLocation.text = resources.getString(
+            R.string.Brewerie_location_info_text,
+            brewerie.address_1,
+            brewerie.city,
+            brewerie.state,
+            brewerie.country
+        )
+
+        binding.openMapsButton.setOnClickListener {
+            openMaps(brewerie.latitude, brewerie.longitude)
+        }
+    }
+
+    private fun openMaps(latitude: String, longitude: String){
+        val gmmIntentUri = Uri.parse("google.streetview:cbll=${latitude},${longitude}")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+
+        startActivity(mapIntent)
     }
 }
