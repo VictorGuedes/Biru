da5be756d775fb7cd058c109ec1ade2c0449621a
Victor Guedes <victoroligue@gmail.com>
2024-06-06 15:12:24 +0100

    initial unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Mockito
+    testImplementation("org.mockito:mockito-core:5.2.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
 }
---
+++ app/src/test/java/com/oligue/app/biru/BrewerieHashMapTest.kt
@@ -0,0 +1,32 @@
+package com.oligue.app.biru
+
+import com.google.android.gms.maps.model.LatLng
+import com.oligue.app.biru.app.main.viewmodel.MainViewModel
+import com.oligue.app.biru.core.model.Brewerie
+import com.oligue.app.biru.core.repository.BrewerieRepository
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class BrewerieHashMapTest {
+
+    private val viewModel = MainViewModel(mock(BrewerieRepository::class.java))
+
+    @Test
+    fun `putBrewerieHashMap should add data to the hashmap`(){
+        val key = LatLng(1.0, 2.0)
+        val brewerie = mock(Brewerie::class.java)
+
+        viewModel.putBrewerieHashMap(key, brewerie)
+
+        assert(viewModel.getBrewerieDataFromHashMap(key) == brewerie)
+    }
+
+    @Test
+    fun `getBrewerieDataFromHashMap should return null for a non-existent key`() {
+        val noExistent = LatLng(2.0, 2.0)
+        assert(viewModel.getBrewerieDataFromHashMap(noExistent) == null)
+    }
+
+
+
+}
